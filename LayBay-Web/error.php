14a79b424ff4d569287d06f8bede81a851204ae1
Philip1718 <127308771+Philip1718@users.noreply.github.com>
2023-11-20 11:34:22 +0200

    update

--- LayBay-Web/error.php
+++
@@ -1,7 +0,0 @@
-<?php if (isset($error) && count($error) > 0) :?>
-    <div class="error">
-        <?php foreach ($error as $err) : ?>
-            <p><?php echo $err ?></p>
-        <?php endforeach ?>
-    </div>
-<?php endif ?>
